1751cac9f7243200061a7d5e4120e060bbc2bdf5
chencl <rainlongchen@gmail.com>
2017-11-08 06:27:25 +0000

    add files

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ubuntu/agv_lf/agv/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ubuntu/axon/devel;/opt/ros/indigo".split(';') if "/home/ubuntu/axon/devel;/opt/ros/indigo" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/agv_lf/agv/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/ubuntu/agv_lf/agv/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/ubuntu/agv_lf/agv/devel/lib:/home/ubuntu/agv_lf/agv/devel/lib/arm-linux-gnueabihf:/home/ubuntu/axon/devel/lib/arm-linux-gnueabihf:/opt/ros/indigo/lib/arm-linux-gnueabihf:/home/ubuntu/axon/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/ubuntu/agv_lf/agv/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/ubuntu/agv_lf/agv/devel/lib/pkgconfig:/home/ubuntu/agv_lf/agv/devel/lib/arm-linux-gnueabihf/pkgconfig:/home/ubuntu/axon/devel/lib/arm-linux-gnueabihf/pkgconfig:/opt/ros/indigo/lib/arm-linux-gnueabihf/pkgconfig:/home/ubuntu/axon/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig:/usr/local/lib/pkgconfig"
+export PWD="/home/ubuntu/agv_lf/agv/build"
+export PYTHONPATH="/home/ubuntu/agv_lf/agv/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/agv_lf/agv/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/ubuntu/agv_lf/agv/src:$ROS_PACKAGE_PATH"
