3d02d468af5dbeb85050006e4b342aa9d459b247
Lingfei <lren002@berkeley.edu>
2017-11-09 11:19:06 +0800

    c++ funky stuff

--- src/axon_link/src/testSerial.cpp
+++ src/axon_link/src/testSerial.cpp
@@ -26,7 +26,8 @@ void commandSend(unsigned char a, unsigned char b, unsigned char c){
 	foo[0] = a;
 	foo[1] = b;
 	foo[2] = c;
-	int b = device.write(foo, 3);
+	int b;
+	b = device.write(foo, 3);
 	cout << b << endl;
 	ROS_INFO("I'm trying to write");
 }
