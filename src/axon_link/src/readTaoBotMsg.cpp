b38655ddf459b3f168794853996da1897be0ff4d
Lingfei <lren002@berkeley.edu>
2018-01-10 00:17:34 +0800

    added a subscribers

---
+++ src/axon_link/src/readTaoBotMsg.cpp
@@ -0,0 +1,30 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <iostream>
+#include <math.h>
+#include "serial/serial.h"
+#include <axon_link/Taobot.h>
+
+using namespace std;
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%d %d %d %d %d %d %d]", msg.motorA_encoder, msg.motorA_dir, msg.motorB_encoder, msg.motorB_dir, msg.motorC_encoder, msg.motorC_dir, voltage);
+}
+
+int main(int argc, char **argv){
+	ros::init(argc, argv, "Taobot_reciever");
+	ros::NodeHandle n;
+
+
+
+	ros::Subscriber sub = n.subscribe("taobot_listener", 1000, taobotCallback);
+
+
+	ros::spin();
+
+
+	return 0;
+
+
+
+}
