3d56912d2ce8a9850bfb44a1668d1e02f9fdc831
Lingfei <lren002@berkeley.edu>
2018-01-10 22:41:15 +0800

    yello

--- src/axon_link/src/eigen.cpp
+++ src/axon_link/src/eigen.cpp
@@ -7,17 +7,18 @@ int main()
   double radius, lengthToCenter, paramA;
   radius = 5;
   paramA = 1/sqrt(3);
+  paramB = 1/3;
   lengthToCenter = 26.3;
   MatrixXd m(3,3);
-  m(0,0) = 3;
+  m(0,0) = 0;
   
-  m(0,1) = -1;
-  m(0,2) = paramA;
-  m(1,0) = 2.5;
-  m(1,1) = m(1,0) + m(0,1);
-  m(1,2) = 0; 
-  m(2,0) = 0;
-  m(2,1) = 0;
-  m(2,2) = 0;
-  std::cout << m << std::endl;
+  m(0,1) = -paramA*radius;
+  m(0,2) = paramA*radius;
+  m(1,0) = -2*paramB*radius;
+  m(1,1) = paramB*radius;
+  m(1,2) = paramB*radius; 
+  m(2,0) = radius*paramB/lengthToCenter;
+  m(2,1) = radius*paramB/lengthToCenter;
+  m(2,2) = radius*paramB/lengthToCenter;
+  std::cout << m.inverse() << std::endl;
 }
