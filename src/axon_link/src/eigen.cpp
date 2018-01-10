e2e777c050f40a8ce7b398b342e879bcda96c132
Lingfei <lren002@berkeley.edu>
2018-01-10 21:58:45 +0800

    yello

--- src/axon_link/src/eigen.cpp
+++ src/axon_link/src/eigen.cpp
@@ -3,10 +3,10 @@
 using Eigen::MatrixXd;
 int main()
 {
-  MatrixXd m(2,2);
+  MatrixXd m(3,3);
   m(0,0) = 3;
   m(1,0) = 2.5;
   m(0,1) = -1;
   m(1,1) = m(1,0) + m(0,1);
-  std::cout << m.inverse()<< std::endl;
+  std::cout << m.inverse() << std::endl;
 }
