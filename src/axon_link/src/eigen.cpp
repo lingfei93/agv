1d0331e53fd6299d1bfd2445fb6151227d7a9dc1
Lingfei <lren002@berkeley.edu>
2018-01-10 22:37:39 +0800

    yello

--- src/axon_link/src/eigen.cpp
+++ src/axon_link/src/eigen.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <cmath>
 using Eigen::MatrixXd;
 int main()
 {
+  double radius, lengthToCenter, paramA;
+  radius = 5;
+  paramA = 1/sqrt(3);
+  lengthToCenter = 26.3;
   MatrixXd m(3,3);
   m(0,0) = 3;
-  m(1,0) = 2.5;
+  
   m(0,1) = -1;
+  m(0,2) = 0;
+  m(1,0) = 2.5;
   m(1,1) = m(1,0) + m(0,1);
+  m(1,2) = 0; 
+  m(2,0) = 0;
+  m(2,1) = 0;
+  m(2,2) = 0;
   std::cout << m << std::endl;
 }
