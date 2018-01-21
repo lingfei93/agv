4d2228935f4d5083ed09156b1c59624b1853a134
Lingfei <lren002@berkeley.edu>
2018-01-22 01:03:43 +0800

    removed the position cos it didnt seem to work super well

--- src/taobot_link/src/sendSpeed.cpp
+++ src/taobot_link/src/sendSpeed.cpp
@@ -340,7 +340,7 @@ int main(int argc, char** argv)
     //comment out this first so i dont keep finding reply
             //keep calling 1 and 3
     //ros::Timer timer2 = n.createTimer(ros::Duration(1.0), callback2);
-    ros::Timer timer1 = n.createTimer(ros::Duration(0.1), callback1); 
+    // ros::Timer timer1 = n.createTimer(ros::Duration(0.1), callback1); 
 
     ros::spin();
 
