e7007e7d86a8a40fc61f8e7360303f9b322a4e71
Lingfei <lren002@berkeley.edu>
2018-01-23 01:17:35 +0800

    update for today; finish odom but need adjust the factor

--- src/taobot_link/src/readTaoBotMsg.cpp
+++ src/taobot_link/src/readTaoBotMsg.cpp
@@ -32,7 +32,7 @@ void taoBotOdomCallback(const taobot_link::Taobot& msg)
     float motorA_incr;
     float motorB_incr;
     float motorC_incr;
-    float magical_factor = 0.5;
+    float magical_factor = 200;
   // ROS_INFO("I heard: [%d %d %d %d %d %d %f]", msg.motorA_encoder, msg.motorA_dir, 
   // 	msg.motorB_encoder, msg.motorB_dir, msg.motorC_encoder, 
   // 	msg.motorC_dir, msg.voltage);
@@ -44,9 +44,9 @@ void taoBotOdomCallback(const taobot_link::Taobot& msg)
     motorB_incr = msg.motorB_encoder * ( msg.motorB_dir - 1 );
     motorC_incr = msg.motorC_encoder * ( msg.motorC_dir - 1 );
 
-    x_pos = x_pos + motorA_incr;
-    y_pos = y_pos + motorB_incr;
-    theta = 0;
+    // x_pos = x_pos + motorA_incr;
+    // y_pos = y_pos + motorB_incr;
+    // theta = theta ;
 
 	double radius, lengthToCenter, paramA, paramB;
 	double x_pos, y_pos, theta;
@@ -91,9 +91,15 @@ void taoBotOdomCallback(const taobot_link::Taobot& msg)
     // theta = output(2,0);
 
     ROS_INFO("%f is X %f is Y %f is theta", output(0,0), output(1,0), output(2,0));
-    odomMsg.x_pos = output(0,0);
-    odomMsg.y_pos = output(1,0);
-    odomMsg.theta = output(2,0);
+
+    x_pos += output(0,0)/magical_factor;
+    y_pos += output(1,0)/magical_factor;
+    theta += output(2,0)/magical_factor;
+
+    odomMsg.x_pos = x_pos;
+    odomMsg.y_pos = y_pos;
+    odomMsg.theta = theta; 
+
     odom_pub.publish(odomMsg);
 
 
