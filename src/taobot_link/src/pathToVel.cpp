5f2ea8b79c9127ac99ae61f3dfa599abbb1fa748
lingfei <lingfei93@gmail.com>
2018-03-31 15:52:59 +0800

    hello

--- src/taobot_link/src/pathToVel.cpp
+++ src/taobot_link/src/pathToVel.cpp
@@ -131,7 +131,7 @@ void movePathCallBack(const nav_msgs::Path::ConstPtr& path_data)
     int len;
     int idx, flag, path_plan;
     //set the idealSpeed as a rando.
-    double idealSpeed = 0.20;
+    double idealSpeed = 0.25;
     double eachTimeSlot;
 
     len = path_data->poses.size();
@@ -300,8 +300,8 @@ void sendVelCommand(float x_start, float y_start, float x_end, float y_end){
     uint8_t reply[30];
 
     //NOT SURE WHY I NEED TO FLIP THIS
-    wlr_cmd.linear.x         = (x_end - x_start) * 0.85;
-    wlr_cmd.linear.y        = (y_end - y_start) * 0.85;
+    wlr_cmd.linear.x         = (x_end - x_start) * 0.75;
+    wlr_cmd.linear.y        = (y_end - y_start) * 0.75;
     wlr_cmd.angular.z = 0;
 
     move_base_path_pub.publish(wlr_cmd);
