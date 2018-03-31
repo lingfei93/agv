db8338c21578296df3f1bd6614ec8f100c3ff6f7
lingfei <lingfei93@gmail.com>
2018-03-31 17:20:55 +0800

    make changes

--- src/taobot_link/src/pathToVel.cpp
+++ src/taobot_link/src/pathToVel.cpp
@@ -300,8 +300,8 @@ void sendVelCommand(float x_start, float y_start, float x_end, float y_end){
     uint8_t reply[30];
 
     //NOT SURE WHY I NEED TO FLIP THIS
-    wlr_cmd.linear.x         = (x_end - x_start) * 0.70;
-    wlr_cmd.linear.y        = (y_end - y_start) * 0.70;
+    wlr_cmd.linear.x         = (x_end - x_start) * 0.73;
+    wlr_cmd.linear.y        = (y_end - y_start) * 0.73;
     wlr_cmd.angular.z = 0;
 
     move_base_path_pub.publish(wlr_cmd);
