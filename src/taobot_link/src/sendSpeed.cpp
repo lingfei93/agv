a7f9f79cbcd438bdc887fe73e9fd2fc4a745bd0d
Lingfei <lren002@berkeley.edu>
2018-04-08 17:40:53 +0800

    helok

--- src/taobot_link/src/sendSpeed.cpp
+++ src/taobot_link/src/sendSpeed.cpp
@@ -184,7 +184,7 @@ uint8_t* changeToOmniSpeed(double verticalPress, double horizontalPress, double 
     ROS_INFO("going up and down");
     //normalize the speed
     motorA_speed = std::abs(motorA_speed)/max;
-    motorB_speed = std::abs(motorB_speed)/max * 1.025;
+    motorB_speed = std::abs(motorB_speed)/max * 1.05;
     motorC_speed = std::abs(motorC_speed)/max;  //in the lab, this value is 1.025 for straight this is the right wheel
     }   
 
